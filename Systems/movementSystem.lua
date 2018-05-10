3254ba0c263de86af1d77e3250cb138a25b73247
MultiPain <32590972+MultiPain@users.noreply.github.com>
2018-05-10 19:38:06 +0900

    Update movementSystem.lua
    Added vector limitation for body velocity

--- Systems/movementSystem.lua
+++ Systems/movementSystem.lua
@@ -10,8 +10,24 @@ end
 
 function MovementSystem:process(e, dt)
 	local body = e.body
-	if (e.flagLeft) then body:applyForceXY(-e.movement.speed, 0, e.movement.maxSpeed) end
-	if (e.flagRight) then body:applyForceXY(e.movement.speed, 0, e.movement.maxSpeed) end
-	if (e.flagUp) then body:applyForceXY(0, -e.movement.speed, e.movement.maxSpeed) end
-	if (e.flagDown) then body:applyForceXY(0, e.movement.speed, e.movement.maxSpeed) end
+	local speed = e.movement.speed
+	
+	if (e.flagLeft) then 
+		body:applyForceXY(-speed, 0)		
+	end
+	
+	if (e.flagRight) then 
+		body:applyForceXY(speed, 0)
+	end
+	
+	if (e.flagUp) then 
+		body:applyForceXY(0, -speed)
+	end
+	
+	if (e.flagDown) then
+		body:applyForceXY(0, speed)
+	end
+	
+	-- limit movement vector
+	body.vel:limit(e.movement.maxSpeed)
 end
